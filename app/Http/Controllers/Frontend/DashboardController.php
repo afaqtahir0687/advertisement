aa2f426ae4e0016a7bdff44e061da10df4985125
Maryam Saleem <maryamsaleem108@gmail.com>
2026-01-28 23:24:32 +0500

    account changes

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Order;
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\Auth;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('frontend.dashboard');
-    }
+        $user = Auth::user();
+        $orders = Order::where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
+        // Assuming Wishlist model has a 'product' relationship
+        $wishlist = Wishlist::where('user_id', $user->id)->with(['product.category', 'product.subcategory'])->get();
 
+        return view('frontend.dashboard', compact('user', 'orders', 'wishlist'));
+    }
 }
--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -159,7 +159,7 @@
                   <span class="mr-2 d-none d-lg-inline text-gray-600 small">
                         {{ Auth::user()->name ?? '' }}
                     </span>
-                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                <img class="img-profile rounded-circle" src="{{ asset('admin/img/undraw_profile.svg') }}">
             </a>
 
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
