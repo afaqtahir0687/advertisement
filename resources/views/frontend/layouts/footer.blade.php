1468e63540d166dc0d18ddf60654ff1ce54a64c0
Murtaza395 <mughalmurtaza999@gmail.com>
2026-02-03 17:16:18 +0500

    footer links add

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,117 +1,121 @@
- <footer class="footer bg-dark">
-            <div class="footer-middle">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="widget">
-                                <h4 class="widget-title">Contact Info</h4>
-                                <ul class="contact-info">
-                                    <li>
-                                        <span class="contact-info-label">Address:</span>Al-Salihiyah District, Arbaeen Street, Riyadh KSA
-                                    </li>
-                                    <li>
-                                        <span class="contact-info-label">Phone:</span><a href="tel:">+966557834154</a>
-                                    </li>
-                                    <li>
-                                        <span class="contact-info-label">Email:</span> <a href="mailto:sale@crelogices.com">sale@crelogices.com</a>
-                                    </li>
-                                    <li>
-                                        <span class="contact-info-label">Working Days/Hours:</span> Saturday - Thursday / 9:00 AM - 9:00 PM
-                                    </li>
-                                </ul>
-                                <div class="social-icons">
-                                    <a href="#" class="social-icon social-facebook icon-facebook" target="_blank" title="Facebook"></a>
-                                    <a href="#" class="social-icon social-twitter icon-twitter" target="_blank" title="Twitter"></a>
-                                    <a href="#" class="social-icon social-instagram icon-instagram" target="_blank" title="Instagram"></a>
-                                </div>
-                                <!-- End .social-icons -->
-                            </div>
-                            <!-- End .widget -->
+<footer class="footer bg-dark">
+    <div class="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="widget">
+                        <h4 class="widget-title">Contact Info</h4>
+                        <ul class="contact-info">
+                            <li>
+                                <span class="contact-info-label">Address:</span>Al-Salihiyah District, Arbaeen Street,
+                                Riyadh KSA
+                            </li>
+                            <li>
+                                <span class="contact-info-label">Phone:</span><a href="tel:">+966557834154</a>
+                            </li>
+                            <li>
+                                <span class="contact-info-label">Email:</span> <a
+                                    href="mailto:sale@crelogices.com">sale@crelogices.com</a>
+                            </li>
+                            <li>
+                                <span class="contact-info-label">Working Days/Hours:</span> Saturday - Thursday / 9:00
+                                AM - 9:00 PM
+                            </li>
+                        </ul>
+                        <div class="social-icons">
+                            <a href="#" class="social-icon social-facebook icon-facebook" target="_blank"
+                                title="Facebook"></a>
+                            <a href="#" class="social-icon social-twitter icon-twitter" target="_blank"
+                                title="Twitter"></a>
+                            <a href="#" class="social-icon social-instagram icon-instagram" target="_blank"
+                                title="Instagram"></a>
                         </div>
-                        <!-- End .col-lg-3 -->
-
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="widget">
-                                <h4 class="widget-title">Customer Service</h4>
-
-                                <ul class="links">
-                                    <li><a href="#">Help & FAQs</a></li>
-                                    <li><a href="#">Order Tracking</a></li>
-                                    <li><a href="#">Shipping & Delivery</a></li>
-                                    <li><a href="#">Orders History</a></li>
-                                    <li><a href="#">Advanced Search</a></li>
-                                    <li><a href="#">My Account</a></li>
-                                    <li><a href="#">Careers</a></li>
-                                    <li><a href="{{ route('about.index') }}">About Us</a></li>
-                                    <li><a href="#">Corporate Sales</a></li>
-                                    <li><a href="#">Privacy</a></li>
-                                </ul>
-                            </div>
-                            <!-- End .widget -->
-                        </div>
-                        <!-- End .col-lg-3 -->
-
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="widget">
-                                <h4 class="widget-title">Popular Tags</h4>
-
-                                <div class="tagcloud">
-                                    <a href="#">Business Cards</a>
-                                    <a href="#">Acrylic</a>
-                                    <a href="#">Signage</a>
-                                    <a href="#">3D Letter</a>
-                                    <a href="#">Sticker</a>
-                                    <a href="#">Banner</a>
-                                    <a href="#">Gift Items</a>
-                                    <a href="#">Pop up</a>
-                                    <a href="#">Roll Up</a>
-                                    <a href="#">Flyer</a>
-                                    <a href="#">Backlit</a>
-                                </div>
-                            </div>
-                            <!-- End .widget -->
-                        </div>
-                        <!-- End .col-lg-3 -->
-
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="widget widget-newsletter">
-                                <h4 class="widget-title">Subscribe newsletter</h4>
-                                <p>Get all the latest information on events, sales and offers. Sign up for newsletter:
-                                </p>
-                                <form action="#" class="mb-0">
-                                    <input type="email" class="form-control m-b-3" placeholder="Email address" required>
-
-                                    <input type="submit" class="btn btn-primary shadow-none" value="Subscribe">
-                                </form>
-                            </div>
-                            <!-- End .widget -->
-                        </div>
-                        <!-- End .col-lg-3 -->
+                        <!-- End .social-icons -->
                     </div>
-                    <!-- End .row -->
+                    <!-- End .widget -->
                 </div>
-                <!-- End .container -->
-            </div>
-            <!-- End .footer-middle -->
+                <!-- End .col-lg-3 -->
 
-            <div class="container">
-                <div class="footer-bottom">
-                    <div class="container d-sm-flex align-items-center">
-                        <div class="footer-left">
-                            <span class="footer-copyright">© Crelogics. 2025. All Rights Reserved</span>
-                        </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="widget">
+                        <h4 class="widget-title">Customer Service</h4>
 
-                        <div class="footer-right ml-auto mt-1 mt-sm-0">
-                            <div class="payment-icons">
-                                <span class="payment-icon visa" style="background-image: url(assets/images/payments/payment-visa.svg)"></span>
-                                <span class="payment-icon paypal" style="background-image: url(assets/images/payments/payment-paypal.svg)"></span>
-                                <span class="payment-icon stripe" style="background-image: url(assets/images/payments/payment-stripe.png)"></span>
-                                <span class="payment-icon verisign" style="background-image:  url(assets/images/payments/payment-verisign.svg)"></span>
-                            </div>
+                        <ul class="links">
+                            <li><a href="{{ route('track.order') }}">Order Tracking</a></li>
+                            <li><a href="{{ route('dashboard') }}">Orders History</a></li>
+                            <li><a href="{{ route('dashboard') }}">My Account</a></li>
+                            <li><a href="{{ route('about.index') }}">About Us</a></li>
+                        </ul>
+                    </div>
+                    <!-- End .widget -->
+                </div>
+                <!-- End .col-lg-3 -->
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="widget">
+                        <h4 class="widget-title">Popular Tags</h4>
+
+                        <div class="tagcloud">
+                            <a href="#">Business Cards</a>
+                            <a href="#">Acrylic</a>
+                            <a href="#">Signage</a>
+                            <a href="#">3D Letter</a>
+                            <a href="#">Sticker</a>
+                            <a href="#">Banner</a>
+                            <a href="#">Gift Items</a>
+                            <a href="#">Pop up</a>
+                            <a href="#">Roll Up</a>
+                            <a href="#">Flyer</a>
+                            <a href="#">Backlit</a>
                         </div>
                     </div>
+                    <!-- End .widget -->
                 </div>
-                <!-- End .footer-bottom -->
+                <!-- End .col-lg-3 -->
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="widget widget-newsletter">
+                        <h4 class="widget-title">Subscribe newsletter</h4>
+                        <p>Get all the latest information on events, sales and offers. Sign up for newsletter:
+                        </p>
+                        <form action="#" class="mb-0">
+                            <input type="email" class="form-control m-b-3" placeholder="Email address" required>
+
+                            <input type="submit" class="btn btn-primary shadow-none" value="Subscribe">
+                        </form>
+                    </div>
+                    <!-- End .widget -->
+                </div>
+                <!-- End .col-lg-3 -->
             </div>
-            <!-- End .container -->
-        </footer>
+            <!-- End .row -->
+        </div>
+        <!-- End .container -->
+    </div>
+    <!-- End .footer-middle -->
+
+    <div class="container">
+        <div class="footer-bottom">
+            <div class="container d-sm-flex align-items-center">
+                <div class="footer-left">
+                    <span class="footer-copyright">© Crelogics. 2025. All Rights Reserved</span>
+                </div>
+
+                <div class="footer-right ml-auto mt-1 mt-sm-0">
+                    <div class="payment-icons">
+                        <span class="payment-icon visa"
+                            style="background-image: url(assets/images/payments/payment-visa.svg)"></span>
+                        <span class="payment-icon paypal"
+                            style="background-image: url(assets/images/payments/payment-paypal.svg)"></span>
+                        <span class="payment-icon stripe"
+                            style="background-image: url(assets/images/payments/payment-stripe.png)"></span>
+                        <span class="payment-icon verisign"
+                            style="background-image:  url(assets/images/payments/payment-verisign.svg)"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End .footer-bottom -->
+    </div>
+    <!-- End .container -->
+</footer>
